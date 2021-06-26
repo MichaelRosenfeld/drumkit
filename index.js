96eb9a41f8d1b28f2e1fc47c4affaa21cddc8dbb
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-06-26 13:04:05 +0200

    Added one Audio File to play instead of alert event on click.Added all background images for instruments.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var audio = new Audio("sounds/tom-1.mp3");
 
 for(var i = 0; i < numberOfDrumButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    alert("I got clicked!");
+    audio.play();
   });
 }
